f1829da8869dbf74170c0d7126679f8e15b6dc11
Daniel <dhuerta1408@gmail.com>
2019-09-19 23:38:08 -0600

    fix some bugs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,87 +1,93 @@
 $(document).ready(function() {
- 
-  var topics = [];
+  showButton();
+
+  $(document).on("click", "#scaryGif", gifState);
+  $(document).on("click", "#movie", getData);
   
-  function horrorMovieGif() {
+});
+
+  
+  function getData(){
+
+      var title = $(this).attr("data-search");
+      var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=A9wDVM3lKqUEjnnlYAa7ZgfpVWLSSaMu&q=" + title + "&limit=10";
+
+      $.ajax({
+        
+        url: queryURL,
+        method: "GET",
+        success: horrorMovieGif,
+        
+      }).then(function(response) {
+        console.log(response);
+      });
     
-    var title = $(this).attr("data-search");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=A9wDVM3lKqUEjnnlYAa7ZgfpVWLSSaMu&q=" + title + "&limit=10";
-    
-    $.ajax({
-      
-      url: queryURL,
-      method: "GET"
-      
-    }).then(function(response) {
-      console.log(response);
-      
+    }  
+
+  function horrorMovieGif(response) {
+      console.log(response.data);
+
       var result = response.data;
       for (i = 0; i < result.length; i++) {
-        
+      
         var gifDiv = $("<div>");
         var movieImage = $("<img>");
         var rating = result[i].rating;
         var p = $("<p>").text("Rating: " + rating);
-        
+      
         movieImage.addClass("scaryGif")
         movieImage.attr("src", result[i].images.fixed_height.url);
         movieImage.attr("src", result[i].images.fixed_height_still.url);
         movieImage.attr("data-still", "still");
-        
+      
         gifDiv.append(movieImage);
         gifDiv.append(p);
-        
+      
         $("#horrorGifs").prepend(gifDiv);
-        
-      }
-    });
-  }
-  
-  $("#addMovie").on("click", function(event) {
-    event.preventDefault()
-    
-    var newMovie = $("#buttonInsert").val().trim();
-    topics.push(newMovie);
-    $("#buttonInsert").val("");
-    
-    showButtons()
-    
-  })
+      
+    };
+  };  
   
   function showButton() {
+    var topics = [];
+    $("#addMovie").on("click", function(event) {
+      event.preventDefault();
+      var newMovie = $("#input1").val().trim();
+      console.log(newMovie);
+      topics.push(newMovie);
     
-    $("#horrorButtons").empty();
-    for (var i =0; i < topics.length; i++) {
+      $("#buttonInsert").val(topics[i]);    
+      showButton();   
+          // $("#horrorButtons").empty();
+    for (var i = 0; i < topics.length; i++) {
       
       var newButton = $("<button class='btn btn-primary'>");
       newButton.attr("id", "movie");
       newButton.attr("data-search", topics[i]);
+      console.log(topics[i]);
       newButton.text(topics[i]);
       $("#horrorButtons").append(newButton);
-      
-    }
-  }
-  
-  showButton()
-  
-  $(document).on("click", "#movie", horrorMovieGif);
-  
-  $(document).on("click", "#scaryGif", gifState);
+      console.log(newButton);
+    };
+    }); 
+    
+
+  };
   
   function gifState() {
     
     var state = $(this).attr("data-state");
-    
+    console.log(response.data.images.original)
     if (state === "still") {
       
-      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("src", $(this).attr("data."));
       $(this).attr("data-state", "animate");
       
     } else {
       
-      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("src", $(this).attr("data-original-still"));
       $(this).attr("data-state", "still");
       
-    }
-  }
-});
+    };
+  };
+
